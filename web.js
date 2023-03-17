1544ea552865f6bb6ecc83b4e5800e9e9656de01
Lora <88323668+llloralyw@users.noreply.github.com>
2023-03-17 18:28:57 +1100

    Add files via upload

---
+++ web.js
@@ -0,0 +1,7 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname))
+var web = app.listen(6060, () =>{
+    console.log('Server is listening on port', web.address().port)
+} )
